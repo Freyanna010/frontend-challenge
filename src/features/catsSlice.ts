79dea87fc34977ed4c979ce38056253ce46f9f5c
Ann <socevilaf@gmail.com>
2025-01-16 12:44:20 +0300

    add slice

---
+++ src/features/catsSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice, PayloadAction,} from "@reduxjs/toolkit";
+import {fetchCatsData } from "./catsThunks";
+import { Cat } from "@/types/types";
+
+
+interface CatsState {
+cats: Cat[];
+isCatsLoading: boolean
+catsError: null | string
+}
+
+const initialState: CatsState = {
+  cats: [],
+  isCatsLoading: false,
+  catsError: null
+};
+
+const catsSlice = createSlice({
+  name: "cats",
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCatsData.pending, (state) => {
+        state.isCatsLoading = true;
+        state.catsError = null;
+      })
+      .addCase(fetchCatsData.fulfilled, (state, action) => {
+        state.isCatsLoading = false;
+        state.cats = action.payload;
+      })
+      .addCase(fetchCatsData.rejected, (state, action) => {
+        state.isCatsLoading = false;
+        state.catsError = action.payload as string;
+      });
+  },
+});
+
+export const {
+
+} = catsSlice.actions;
+export default catsSlice.reducer;
--- src/features/catsThunks.ts
+++ src/features/catsThunks.ts
@@ -1,33 +1,30 @@
 import { API_KEY, API_URL } from "@/config/config";
+import { Cat } from "@/types/types";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
-type ErrorResponse = {
-  message: string;
-};
-
-export const fetchStudentsData = createAsyncThunk(
-  "cats/fetchCats",
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await axios.get(API_URL, {
-        headers: {
-          "x-api-key": API_KEY,
-        },
-        params: {
-          limit: 100,
-        },
-      });
-      return response.data;
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError<ErrorResponse>;
-        return rejectWithValue(
-          axiosError.response?.data?.message || "Ошибка запроса котиков"
-        );
-      } else {
-        return rejectWithValue("Ошибка сети");
-      }
+export const fetchCatsData = createAsyncThunk<
+  Cat[],
+  void,
+  { rejectValue: string }
+>("cats/fetchCatsData", async (_, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(API_URL, {
+      headers: {
+        "x-api-key": API_KEY,
+      },
+      params: {
+        limit: 100,
+      },
+    });
+    return response.data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      return rejectWithValue(
+        error.response?.data?.message || "Ошибка запроса котиков"
+      );
+    } else {
+      return rejectWithValue("Неизвестная ошибка");
     }
   }
-);
+});
