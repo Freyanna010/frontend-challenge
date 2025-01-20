f08f9c33e782a6827ef61fa4547128565c63b438
Ann <socevilaf@gmail.com>
2025-01-20 12:45:16 +0300

    add browserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { Provider as StoreProvider } from "react-redux";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 
 import "./index.css";
 import App from "./component/App/App.tsx";
 import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <StoreProvider store={store}>
+  <BrowserRouter basename="/frontend-challenge">
+    <StoreProvider store={store}>
     <App />
   </StoreProvider>,
+  </BrowserRouter>
+
 );
